7c7e8395a16d5de4aabeb826571564c43dc39bff
Catie Lanier <hello@catielanier.ca>
2024-01-29 16:13:10 -0500

    add monogram to dividers

--- src/components/Divider.svelte
+++ src/components/Divider.svelte
@@ -2,31 +2,42 @@
     export let anchorId;
 </script>
 
-<div class="divider div-transparent" id={anchorId}>
-
+<div class="divider" id={anchorId}>
+    <div class="div-transparent" />
+    <img src="../assets/monogram.png" alt="monogram">
 </div>
 
 <style>
     .divider {
 		margin: 6rem 0;
 		position: relative;
-		height: 2px;
+		height: 3.5rem;
 	}
 
-	.div-transparent:before {
-		content: "";
+	.div-transparent {
+		content: " ";
 		position: absolute;
-		top: 0;
+		top: 50%;
 		left: 0;
 		right: 0;
+        transform: translateY(-50%);
 		width: 100%;
 		height: 2px;
 		background-image: linear-gradient(
 			to right,
 			transparent,
 			rgb(194, 132, 133),
+			transparent,
+			rgb(194, 132, 133),
 			transparent
 		);
 	}
+    .divider img {
+        height: 3.5rem;
+        position: absolute;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+    }
 
 </style>
